31fa09225cd92a6db02b435a1933fbff631f65c1
omatviiv <oleh.matviiv@gmail.com>
2023-10-29 19:45:30 +0100

    ensure that test files will not go into bundle even if imported

--- webpack/base.config.js
+++ webpack/base.config.js
@@ -17,7 +17,24 @@ module.exports = {
     strictExportPresence: true,
     rules: [
       {
-        test: /\.tsx?$/,
+        // this only matches filenames that end with .ts or .tsx
+        // but not with test.ts or test.tsx
+        // WHY DO WE NEED IT: to ensure that test files newer appear in bundle
+        // this solution tested using webpack-bundle-analyzer by creating
+        // file wich ends with test.ts and importing from it something
+        // for example a constant, then running npm run analyze-build
+        // and as the result number of concatenated files does include
+        // that file ending test.ts but its content is not included
+        // this can be visible by src folder size in bundle: it was the same
+        // as before modifying regex to skip *.test.tsx? files
+        // also tested that with old regex that accepts all *.tsx? files
+        // src folder size in bundel does inclrease
+        // Why number of concatenated files is still affected - because
+        // webpackConfig.resolve does allow all *.ts or *.tsx files and
+        // regexes are not supported there plus maybe we even don't need this
+        // because without this imports in test files themselves might stop
+        // working
+        test: /^.*[^t][^e][^s][^t]\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/,
         include: [
